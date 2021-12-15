1ff68e8e60f8f75ca84e2aaf042e7e1900a2ab7a
Alan Reineri <alan@mentat.solutions>
2021-12-15 10:32:55 -0300

    init: Create next app

---
+++ .history/src/pages/index_20211215102351.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Head> 
+      <title>App - Counter</title>
+    </Head>
+  <h1>Text</h1>
+  )
+}
